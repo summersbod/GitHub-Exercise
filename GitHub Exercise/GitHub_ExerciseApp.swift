d5770ae5523ac40b1df3d358e194d26797ee8421
Shrenik Vora <summers.bodegas.0s@icloud.com>
2024-12-05 12:42:29 -0600

    Initial Commit

---
+++ GitHub Exercise/GitHub_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHub_ExerciseApp.swift
+//  GitHub Exercise
+//
+//  Created by SHRENIK VORA on 12/5/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHub_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
